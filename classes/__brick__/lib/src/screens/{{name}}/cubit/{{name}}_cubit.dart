110626a6b75610c9797bb0dfd353b6a8420149c1
Ammar Ayoubi <59621670+ammarayoubi@users.noreply.github.com>
2022-08-17 17:08:58 +0500

    template configuration

--- classes/__brick__/lib/src/screens/{{name}}/cubit/{{name}}_cubit.dart
+++ classes/__brick__/lib/src/screens/{{name}}/cubit/{{name}}_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-part '{{name}}__state.dart';
+part '{{name}}_state.dart';
 
 class {{name.pascalCase()}}Cubit extends Cubit<{{name.pascalCase()}}State> {
       {{name.pascalCase()}}Cubit() : super({{name.pascalCase()}}Initial());
